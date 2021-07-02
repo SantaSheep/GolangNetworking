60a4b54b06ff4cb0d1d91ead93e04287b939a3c5
Peter Smith <superhotfire2018@gmail.com>
2021-07-02 13:13:45 -0700

    Finished chapter 8

---
+++ ch08/block_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func blockIndefinitely(w http.ResponseWriter, r *http.Request) {
+	select {}
+}
+
+func TestBlockIndefinitely(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(blockIndefinitely))
+	_, _ = http.Get(ts.URL)
+	t.Fatal("client did not indefinitely block")
+}
+
+func TestBlockIndefinitelyWithTimeout(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(blockIndefinitely))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatal(err)
+		}
+		return
+	}
+	_ = resp.Body.Close()
+}
